55ad7b1116d45bd5024f4c37f1f5a60b426d2325
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2019-03-16 20:36:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,6 +5,8 @@ class App < Sinatra::Base
   get '/food_form' do
     erb :food_form
   end
-  # Add your post route and action below
+  post '/food' do # receives data from HTML
+params.to_s
+  end             # Add your post route and action below
 
 end
